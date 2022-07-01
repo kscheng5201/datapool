9a36d35f8a03db3ac057bfd858d6833da0993851
kscheng5201 <105192036+kscheng5201@users.noreply.github.com>
2022-07-01 14:31:58 +0800

    drop tag and tracker

--- sh/tag/tag/run_daily_all.sh
+++
@@ -1,6 +0,0 @@
-
-
-/root/anaconda3/bin/python /root/datapool/sh/tag/tag/run_cdp_trigger_first.py
-/root/anaconda3/bin/python /root/datapool/sh/tag/tag/run_nix_self_first.py
-/root/anaconda3/bin/python /root/datapool/sh/tag/tag/run_nix_trigger_first.py
-
