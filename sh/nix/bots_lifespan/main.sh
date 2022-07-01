84ef7b66e8db7fac35f4e00dda58c46c856309d3
kscheng5201 <105192036+kscheng5201@users.noreply.github.com>
2022-07-01 14:29:10 +0800

    add mkdir

--- sh/nix/bots_lifespan/main.sh
+++ sh/nix/bots_lifespan/main.sh
@@ -1,4 +1,10 @@
 
+# 建立資料夾路徑
+mkdir -p /root/datapool/sh/nix/
+mkdir -p /root/datapool/export_file/nix/
+mkdir -p /root/datapool/error_log/nix/
+
+
 # 更新機器人的資訊
 sh /root/datapool/sh/nix/bots_lifespan/bots_detail.sh
 
--- sh/nix/nes_model/main.sh
+++ sh/nix/nes_model/main.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/python
 
+
+# 建立資料夾路徑
+mkdir -p /root/datapool/sh/nix/
+mkdir -p /root/datapool/export_file/nix/
+mkdir -p /root/datapool/error_log/nix/
+
+
 /root/anaconda3/bin/python /root/datapool/sh/nix/nes_model/nix_check_have_db_or_not.py 2>>/root/datapool/error_log/nix/nes_model/nix_check_have_db_or_not.error 
 /root/anaconda3/bin/python /root/datapool/sh/nix/nes_model/nix_table_NES_Model_daily.py 2>>/root/datapool/error_log/nix/nes_model/nix_table_NES_Model_daily.error 
 
